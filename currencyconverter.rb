a6c184ed11a7fb456c4cbb6eb655c0fb7d3fb077
LylyG <lgalarza@spoonflower.com>
2016-02-06 17:36:30 -0500

    Convert from USD to other currency type. Now convert from non-USD to other non-USD

--- currencyconverter.rb
+++ currencyconverter.rb
@@ -2,42 +2,32 @@ require 'byebug'
 require './currency.rb'
 
 class CurrencyConverter
-  def initialize (symbol)
-    @symbol = symbol
-    @amount = amount
-    @new_total = new_total
+  def initialize (money, desired_rate)
+    @money = money
     @desired_rate = desired_rate
-    hash = {USD: 1.0, EUR: 0.89, RUB: 0.013,
-     INR: 0.015, CNY: 0.15, JPY: 0.0086, BRL: 0.25}
-  end
-
-  def symbol
-    @symbol
-  end
-
-  def amount
-    @amount.to_f
-  end
-
-  def new_total
-    @new_total
+    @hash = {USD: 1.0, EUR: 0.89, RUB: 0.013,
+            INR: 0.015, CNY: 0.15, JPY: 0.0086, BRL: 0.25}
   end
 
   def desired_rate
     @desired_rate
   end
 
-  def +(desired_rate)
-    @new_total + desired_rate
+  # def money
+  #   @money
+  # end
+
+  def hash
+    @hash
   end
 
-  def to_s
-     @new_total
-  end
+  # def amount
+  #   @amount
+  # end
 
-  def convert(amount)
-    # new_total = Currency.new(wallet1.amount * hash[desired_rate])
-    new_total = Currency.new((wallet1.amount * hash[desired_rate]).to_s)
-    puts "#{new_total}" , "#{desired_rate}"
+  def convert(money, desired_rate)
+    new_amount = money.to_f * @hash[desired_rate]
+    new_total = Currency.new(new_amount.to_f, desired_rate)
+    puts new_total.amount, desired_rate
   end
 end
--- execute.rb
+++ execute.rb
@@ -2,20 +2,6 @@ require 'byebug'
 require './currency.rb'
 require './currencyconverter.rb'
 
-# puts wallet.convert(Currency.new(1, :USD), :USD) == Currency.new(1, :USD)
-
-hash = {USD: 1.0, EUR: 0.89, RUB: 0.013,
-   INR: 0.015, CNY: 0.15, JPY: 0.0086, BRL: 0.25}
-
-desired_rate = :BRL
-
-wallet1 = Currency.new("$20")
-new_total = wallet1.amount * hash[desired_rate]
-#
-puts new_total, desired_rate.to_s
-# #
-# #  def to_s
-# #     new_total
-# #  end
-# #
-  # puts wallet1.convert
+money = CurrencyConverter.new(20, :USD)
+# money = Currency.new(20, :USD)
+puts money.convert(20, :BRL)
